31a403bd2b7549f65d5d6cb68a4f6bd0ed209d96
Bryan Davis <bd808@php.net>
2012-03-24 23:35:53 +0000

    Make test runner script fail if any test fails.

--- test.sh
+++ test.sh
@@ -2,12 +2,15 @@
 # Run phpt tests
 #
 
+set -e
+set -u
+
 TEST=${1:-tests/}
 
 USE_PHP=${TEST_PHP_EXECUTABLE:-/usr/bin/php}
 
 TEST_PHP_EXECUTABLE=${USE_PHP} \
+REPORT_EXIT_STATUS=1 \
 ${USE_PHP} \
   -n -d open_basedir= -d output_buffering=0 -d memory_limit=-1 \
   run-tests.php -n -d extension_dir=modules -d extension=yaml.so ${TEST}
-  #-d extension_dir=modules -d extension=yaml.so \
