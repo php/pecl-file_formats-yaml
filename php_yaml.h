11fb7758d52cbc930a485d8e765ff6826a537f1d
Anatol Belski <ab@php.net>
2015-10-17 17:25:53 +0200

    fix data types

--- php_yaml.h
+++ php_yaml.h
@@ -76,12 +76,12 @@ extern zend_module_entry yaml_module_entry;
 
 ZEND_BEGIN_MODULE_GLOBALS(yaml)
 	zend_bool decode_binary;
-	long decode_timestamp;
+	zend_long decode_timestamp;
 	zend_bool decode_php;
 	zval *timestamp_decoder;
 	zend_bool output_canonical;
-	long output_indent;
-	long output_width;
+	zend_long output_indent;
+	zend_long output_width;
 #ifdef IS_UNICODE
 	UConverter *orig_runtime_encoding_conv;
 #endif
