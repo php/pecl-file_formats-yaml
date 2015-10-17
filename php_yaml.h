1d8209292667cc656b638cd81ad36f3e2958a185
Bryan Davis <bd808@php.net>
2015-10-17 14:57:19 -0600

    Release 2.0.0RC5

--- php_yaml.h
+++ php_yaml.h
@@ -68,7 +68,7 @@ extern "C" {
 extern "C" {
 #endif
 
-#define PHP_YAML_VERSION "2.0.0RC4"
+#define PHP_YAML_VERSION "2.0.0RC5"
 
 extern zend_module_entry yaml_module_entry;
 #define phpext_yaml_ptr &yaml_module_entry
