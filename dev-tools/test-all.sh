debe91935cbc9e21dadf17921420a4dbca18a4ee
Bryan Davis <bd808@bd808.com>
2017-07-29 09:29:26 -0600

    dev-tools: only test with PHP7 versions
    Filter the list of phpenv versions to only include the ones that start
    with "7".

--- dev-tools/test-all.sh
+++ dev-tools/test-all.sh
@@ -2,10 +2,10 @@
 
 rm .rbenv-version 2>/dev/null
 
-VERSIONS=$(phpenv versions --bare)
+VERSIONS=$(phpenv versions --bare | grep ^7)
 SCRIPT_DIR=$(dirname $0)
 
-for v in $VERSIONS system; do
+for v in $VERSIONS; do
   echo -n "$v: "
   if ${SCRIPT_DIR}/test-build.sh $v >$v.log 2>&1; then
     echo -e "\033[1;32mPASS\033[0m"
