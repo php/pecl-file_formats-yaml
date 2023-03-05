cb7831731b9f45863e36676dcf53acf66a29f8c8
Bryan Davis <bd808@bd808.com>
2023-03-05 11:01:46 -0700

    Make a 2.2.3 release
    Bugs Fixed:
    - Fixed memory release on error handling (negram)
    - Fix test for PHP 8.2 (andypost)
    - #65: Fix #65 yaml_parse_url method not working (remicollet)

--- php_yaml.h
+++ php_yaml.h
@@ -70,7 +70,7 @@ extern "C" {
 extern "C" {
 #endif
 
-#define PHP_YAML_VERSION "2.2.2"
+#define PHP_YAML_VERSION "2.2.3"
 
 extern zend_module_entry yaml_module_entry;
 #define phpext_yaml_ptr &yaml_module_entry
