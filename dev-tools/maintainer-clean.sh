aa8e943b4b38d766fd2767b6a3f1891605daa663
Bryan Davis <bd808@bd808.com>
2025-11-11 20:04:28 -0700

    Developer tool updates
    - .gitignore: Ignore *.dep files generated by build
    - dev-tools/maintainer-clean.sh: Update to match `make distclean`
    - dev-tools/maintainer-clean.sh: Delete more generated files
    - dev-tools/test-build.sh: Allow selecting YAML dir with LIBYAML envvar

--- dev-tools/maintainer-clean.sh
+++ dev-tools/maintainer-clean.sh
@@ -8,13 +8,22 @@ cd "${WORK_DIR}"
 
 ## what `make distclean` does
 find . -name \*.gcno -o -name \*.gcda | xargs rm -f
-find . -name \*.lo -o -name \*.o | xargs rm -f
+find . -name \*.lo -o -name \*.o -o -name \*.dep | xargs rm -f
 find . -name \*.la -o -name \*.a | xargs rm -f
 find . -name \*.so | xargs rm -f
 find . -name .libs -a -type d|xargs rm -rf
-rm -f libphp.la   modules/* libs/*
-rm -f Makefile config.cache config.log config.status Makefile.objects Makefile.fragments libtool main/php_config.h stamp-h php5.spec sapi/apache/libphp.module buildmk.stamp
-[ -f configure ] && grep -E define'.*include/php' configure | sed 's/.*>//'|xargs rm -f
+rm -f libphp.la        modules/* libs/*
+rm -f ext/opcache/jit/ir/gen_ir_fold_hash
+rm -f ext/opcache/jit/ir/minilua
+rm -f ext/opcache/jit/ir/ir_fold_hash.h
+rm -f ext/opcache/jit/ir/ir_emit_x86.h
+rm -f ext/opcache/jit/ir/ir_emit_aarch64.h
+rm -f Makefile config.cache config.log config.status Makefile.objects Makefile.fragments libtool main/php_config.h main/internal_functions_cli.c main/internal_functions.c Zend/zend_dtrace_gen.h Zend/zend_dtrace_gen.h.bak Zend/zend_config.h
+rm -f main/build-defs.h scripts/phpize
+rm -f ext/date/lib/timelib_config.h ext/mbstring/libmbfl/config.h
+rm -f scripts/man1/phpize.1 scripts/php-config scripts/man1/php-config.1 sapi/cli/php.1 sapi/cgi/php-cgi.1 sapi/phpdbg/phpdbg.1 ext/phar/phar.1 ext/phar/phar.phar.1
+rm -f sapi/fpm/php-fpm.conf sapi/fpm/init.d.php-fpm sapi/fpm/php-fpm.service sapi/fpm/php-fpm.8 sapi/fpm/status.html
+rm -f ext/phar/phar.phar ext/phar/phar.php
 
 ## get rid of anything that phpize and configure made as well
 rm -rf .deps \
@@ -35,7 +44,9 @@ config.nice \
 config.status \
 config.sub \
 configure \
+configure.ac \
 configure.in \
+configure~ \
 include \
 install-sh \
 libtool \
@@ -49,4 +60,4 @@ run-tests.php \
 rm -rf coverage.info reports
 
 ## failed tests
-rm tests/*.{diff,exp,log,out,php,sh}
+rm -f tests/*.{diff,exp,log,out,php,sh}
